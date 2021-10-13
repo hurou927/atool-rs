b9b1f31ef5e63f6ba87f35699e97c9141943aac9
hurou927 <god.be.with.ye.fs@gmail.com>
2021-10-13 21:46:04 +0900

    add test

--- src/archives/tarbz2.rs
+++ src/archives/tarbz2.rs
@@ -3,27 +3,31 @@ use tokio::process::Command;
 
 use super::archive::{Archive, Src};
 
-pub struct TarBz2<'a> {
-    filename_suffix: &'a str,
-    file_cmd_result: Regex,
+pub struct TarBz2 {
+    filename_regex: Regex,
+    file_cmd_regex: Regex,
 }
 
-impl<'a> TarBz2<'a> {
+impl TarBz2 {
     pub fn new() -> Self {
+        let r_filename = r".*\.tar\.bz2$";
+
+        let r_file_cmd = r#"^(GNU|POSIX) tar archive \(bzip2 compressed data(\W|$)/"#;
+
         TarBz2 {
-            filename_suffix: "hoge",
-            file_cmd_result: Regex::new("^hoge$").unwrap(),
+            filename_regex: Regex::new(r_filename).unwrap(),
+            file_cmd_regex: Regex::new(r_file_cmd).unwrap(),
         }
     }
 }
 
-impl<'a> Archive for TarBz2<'a> {
+impl Archive for TarBz2 {
     fn from_filename(&self, filename: &str) -> bool {
-        filename.ends_with(self.filename_suffix)
+        self.filename_regex.is_match(filename)
     }
 
     fn from_file_cmd(&self, stdout: &str) -> bool {
-        self.file_cmd_result.is_match(stdout)
+        self.file_cmd_regex.is_match(stdout)
     }
 
     fn ls(&self, src: Src) -> Command {
@@ -31,5 +35,19 @@ impl<'a> Archive for TarBz2<'a> {
     }
     fn pack(&self, src: Src) -> Command {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::archives::{archive::Archive, tarbz2::TarBz2};
+
+    #[test]
+    fn from_filename() {
+        let archive = TarBz2::new();
+        assert_eq!(archive.from_filename("/tmp/hoge.tar.bz2"), true);
+        assert_eq!(archive.from_filename("/tmp/hoge.tar.gz"), false);
+        assert_eq!(archive.from_filename("/tmp/hoge.bz2"), false);
+        assert_eq!(archive.from_filename("/tmp/hogetar..bz2"), false);
     }
 }
