65a09de6937550c848b51708dda3b91a32a91de0
hurou927 <god.be.with.ye.fs@gmail.com>
2021-10-17 22:28:25 +0900

    move method to archive

--- src/archives/archive.rs
+++ src/archives/archive.rs
@@ -1,6 +1,6 @@
 use tokio::process::Command;
 
-use crate::archives::tarbz2::TarBz2;
+use crate::{archives::tarbz2::TarBz2, ra_error::RaError};
 
 pub struct Src<'a> {
     pub src_path: &'a str,
@@ -15,4 +15,34 @@ pub trait Archive {
 
 pub fn archive_list() -> Vec<Box<dyn Archive>> {
     vec![Box::new(TarBz2::new())]
+}
+
+async fn detect_by_file_cmd<'a>(
+    file_path: &str,
+    archives: &'a Vec<Box<dyn Archive>>,
+) -> Option<&'a Box<dyn Archive>> {
+    let output = Command::new("file")
+        .arg("-z")
+        .arg("-L")
+        .arg("-b")
+        .arg(file_path)
+        .output()
+        .await
+        .unwrap();
+    let result = std::str::from_utf8(&output.stdout).unwrap();
+    archives.iter().find(|x| x.from_file_cmd(result))
+}
+
+pub async fn get_archive<'a>(
+    file_path: &'a str,
+    archives: &'a Vec<Box<dyn Archive>>,
+) -> Result<&'a Box<dyn Archive>, RaError> {
+    match archives.iter().find(|x| x.from_filename(file_path)) {
+        Some(x) => Ok(x),
+        None => detect_by_file_cmd(file_path, archives)
+            .await
+            .ok_or(RaError::UnSupportedFormat {
+                path: file_path.to_string(),
+            }),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,8 @@ mod ra_error;
 
 use std::error::Error;
 
-use archives::archive::Archive;
-use ra_error::RaError;
-use tokio::process::Command;
-
 use crate::{
-    archives::archive::{archive_list, Src},
+    archives::archive::{archive_list, get_archive, Src},
     helpers::Helpers,
 };
 use clap::Clap;
@@ -38,41 +34,6 @@ struct Ls {
     debug: bool,
 
     source: String,
-}
-
-async fn from_file_cmd<'a>(
-    file_path: &str,
-    archives: &'a Vec<Box<dyn Archive>>,
-) -> Option<&'a Box<dyn Archive>> {
-    let output = Command::new("file")
-        .arg("-z")
-        .arg("-L")
-        .arg("-b")
-        .arg(file_path)
-        .output()
-        .await
-        .unwrap();
-    let result = std::str::from_utf8(&output.stdout).unwrap();
-    archives.iter().find(|x| x.from_file_cmd(result))
-}
-
-async fn get_archive<'a>(
-    file_path: &'a str,
-    archives: &'a Vec<Box<dyn Archive>>,
-) -> Result<&'a Box<dyn Archive>, RaError> {
-    match archives.iter().find(|x| x.from_filename(file_path)) {
-        Some(x) => Ok(x),
-        None => from_file_cmd(file_path, archives)
-            .await
-            .ok_or(RaError::UnSupportedFormat {
-                path: file_path.to_string(),
-            }),
-    }
-}
-
-fn to_string(bin: &Vec<u8>) -> Result<String, Box<dyn Error>> {
-    let a = std::str::from_utf8(bin)?;
-    Ok(a.to_string())
 }
 
 #[tokio::main]
